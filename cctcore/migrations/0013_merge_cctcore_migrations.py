0b22c2879e6252ad5658cb4e0fa3a1ef3a455669
Busca CCT Dev <dev@buscacct.local>
2026-08-18 15:49:52 -0300

    fix: merge parallel cctcore migrations

---
+++ cctcore/migrations/0013_merge_cctcore_migrations.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("cctcore", "0008_documentocct_campos_operacionais"),
+        ("cctcore", "0012_add_campos_manuais_documento_cct"),
+    ]
+
+    operations = []
